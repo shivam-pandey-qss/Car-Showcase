6288be18e65e0a44fdaa0db42699fb31ebca4149
Shivam Pandey <shivam.pandey@qsstechnosoft.com>
2023-09-08 18:14:17 +0530

    Added more UI component

---
+++ components/CustomButton.tsx
@@ -0,0 +1,25 @@
+'use client';
+import { CustomButtonProps } from '@/types';
+import Image from 'next/image';
+
+const CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+  btnType,
+  textStyles,
+  rightIcon,
+}: CustomButtonProps) => {
+  return (
+    <button
+      disabled={false}
+      type={btnType || 'button'}
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
+    >
+      <span className={`flex - 1 ${textStyles}`}>{title}</span>
+    </button>
+  );
+};
+
+export default CustomButton;
