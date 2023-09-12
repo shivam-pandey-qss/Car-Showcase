9212b48812a7d07808d8c30e734d56c1d1113459
Shivam Pandey <shivam.pandey@qsstechnosoft.com>
2023-09-12 18:33:52 +0530

    added filter gas and year

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import { CarCard, CustomFilter, Hero, SearchBar } from '@/components';
+import ShowMore from '@/components/ShowMore';
+import { fuels, yearsOfProduction } from '@/constants';
 import { fetchCars } from '@/utils';
 import Image from 'next/image';
 
@@ -22,8 +24,8 @@ export default async function Home({ searchParams }) {
         <div className='home__filters'>
           <SearchBar />
           <div className='home__filter-container'>
-            <CustomFilter title='fuel' />
-            <CustomFilter title='year' />
+            <CustomFilter title='fuel' options={fuels} />
+            <CustomFilter title='year' options={yearsOfProduction} />
           </div>
         </div>
         {!isDataEmpty ? (
@@ -33,6 +35,10 @@ export default async function Home({ searchParams }) {
                 <CarCard car={car} />
               ))}
             </div>
+            <ShowMore
+              pageNumber={(searchParams.pageNumber || 10) / 10}
+              isNext={searchParams.limit || 10 > 10}
+            />
           </section>
         ) : (
           <div className='home__error-container'>
--- utils/index.ts
+++ utils/index.ts
@@ -1,12 +1,13 @@
-import { CarProps } from '@/types';
+import { CarProps, FilterProps } from '@/types';
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+  const { manufacturer, year, model, limit, fuel } = filters;
   const headers = {
     'X-RapidAPI-Key': '81c923b3ffmsh3889f91d7b6d41ep15f35ajsnc3afcc426658',
     'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
   };
   const response = await fetch(
-    'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3',
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
     { headers: headers }
   );
 
@@ -41,4 +42,11 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
   url.searchParams.append('angle', `${angle}`);
 
   return `${url}`;
+};
+
+export const updateSearchParams = (type: string, value: string) => {
+  const searchParams = new URLSearchParams(window.location.search);
+  searchParams.set(type, value);
+  const newPathName = `${window.location.pathname}?${searchParams.toString()}`;
+  return newPathName;
 };
