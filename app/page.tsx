1c1e69b6a1f5a28e398f76ad79604dabbb3b757b
Shivam Pandey <shivam.pandey@qsstechnosoft.com>
2023-09-13 09:59:47 +0530

    Web app completed using server side rendering

--- app/page.tsx
+++ app/page.tsx
@@ -36,8 +36,8 @@ export default async function Home({ searchParams }) {
               ))}
             </div>
             <ShowMore
-              pageNumber={(searchParams.pageNumber || 10) / 10}
-              isNext={searchParams.limit || 10 > 10}
+              pageNumber={(searchParams.limit || 10) / 10}
+              isNext={searchParams.limit || 10 > allCars.length}
             />
           </section>
         ) : (
